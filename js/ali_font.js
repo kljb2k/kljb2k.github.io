74c7117ffdabdfb79ee9a9407ced1ab19e3df961
kele <1617262794@qq.com>
2022-09-22 22:35:37 +0800

    Site updated: 2022-09-22 22:35:36

---
+++ js/ali_font.js
@@ -0,0 +1,95 @@
+@font-face {
+    font-family: "iconfont"; /* Project id 3418531 */
+    src: url('//at.alicdn.com/t/c/font_3418531_1nj2tn2200u.woff2?t=1663834506772') format('woff2'),
+         url('//at.alicdn.com/t/c/font_3418531_1nj2tn2200u.woff?t=1663834506772') format('woff'),
+         url('//at.alicdn.com/t/c/font_3418531_1nj2tn2200u.ttf?t=1663834506772') format('truetype');
+  }
+  
+  .iconfont {
+    font-family: "iconfont" !important;
+    font-size: 16px;
+    font-style: normal;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  .icon-xiaolaoshu:before {
+    content: "\e6ae";
+  }
+  
+  .icon-xiaoniuniu:before {
+    content: "\e6b1";
+  }
+  
+  .icon-xiaolaohu:before {
+    content: "\e6b9";
+  }
+  
+  .icon-xiaotuzi:before {
+    content: "\e6ba";
+  }
+  
+  .icon-xiaosheshe:before {
+    content: "\e6bb";
+  }
+  
+  .icon-xiaoma:before {
+    content: "\e6bc";
+  }
+  
+  .icon-xiaoyangyang:before {
+    content: "\e6bd";
+  }
+  
+  .icon-xiaohouzi:before {
+    content: "\e6be";
+  }
+  
+  .icon-xiaoji:before {
+    content: "\e6bf";
+  }
+  
+  .icon-xiaogouzi:before {
+    content: "\e6c0";
+  }
+  
+  .icon-xiaozhuzhu:before {
+    content: "\e6c1";
+  }
+  
+  .icon-xiaohema:before {
+    content: "\e6c2";
+  }
+  
+  .icon-xiaohuli:before {
+    content: "\e6c3";
+  }
+  
+  .icon-xiaoqie:before {
+    content: "\e6c4";
+  }
+  
+  .icon-xiaoshizi:before {
+    content: "\e6c5";
+  }
+  
+  .icon-xiaolu:before {
+    content: "\e6c6";
+  }
+  
+  .icon-xiaomaomao:before {
+    content: "\e6c7";
+  }
+  
+  .icon-xiaoqingwa:before {
+    content: "\e6c8";
+  }
+  
+  .icon-xiaoxiang:before {
+    content: "\e6c9";
+  }
+  
+  .icon-xiaoxiongmao:before {
+    content: "\e6ca";
+  }
+  
